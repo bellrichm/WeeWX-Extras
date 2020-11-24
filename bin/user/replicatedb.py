be031a3ed7bf0f2d7a30c06d1cbad421e047e68b
Rich Bell <bellrichm@gmail.com>
2020-11-24 16:17:34 -0500

    initial checkin

---
+++ bin/user/replicatedb.py
@@ -0,0 +1,62 @@
+""" Replicate a SQLite DB. """
+
+# todo - mainline routine to fill in 'holes' of secondary db
+
+import weewx
+
+class ReplicateDB(weewx.engine.StdService):
+    """ Replicate a SQLite db. """
+    def __init__(self, engine, config_dict):
+        super(ReplicateDB, self).__init__(engine, config_dict)
+
+        self.config_dict = config_dict
+        service_dict = config_dict.get('ReplicateDB', {})
+        self.databases = []
+        for section in service_dict.sections:
+            db_dict = service_dict.get(section, {})
+            database = {}
+            database['name'] = section
+            database['event_catchup'] = db_dict.get('event_catchup', False)
+            database['primary_binding'] = db_dict['primary_binding']
+            database['secondary_binding'] = db_dict['secondary_binding']
+            database['secondary_dbm'] = weewx.manager.open_manager_with_config(
+                config_dict,
+                database['secondary_binding'],
+                initialize=True)
+            self.databases.append(database)
+
+        self.bind(weewx.STARTUP, self.startup)
+        self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
+
+        print("catching up")
+        for database in self.databases:
+            if not database['event_catchup']:
+                self._replicate(database['primary_binding'], database['secondary_dbm'])
+
+    def startup(self, event): # Need to match signature pylint: disable=unused-argument
+        """ WeeWX startup event. """
+        for database in self.databases:
+            if database['event_catchup']:
+                self._create_events(database['primary_binding'], database['secondary_dbm'])
+
+    def new_archive_record(self, event): # Need to match signature pylint: disable=unused-argument
+        """ WeeWX new archive record event. """
+        # todo, use the record on the event
+        for database in self.databases:
+            self._replicate(database['primary_binding'], database['secondary_dbm'])
+
+    def _create_events(self, primarydb_binding, secondary_dbm):
+        last_good_time = secondary_dbm.lastGoodStamp()
+        primary_dbm = weewx.manager.open_manager_with_config(self.config_dict, primarydb_binding)
+        for record in primary_dbm.genBatchRecords(last_good_time):
+            self.engine.dispatchEvent(weewx.Event(weewx.NEW_ARCHIVE_RECORD,
+                                                  record=record,
+                                                  origin='hardware'))
+
+    def _replicate(self, primarydb_binding, secondary_dbm):
+        last_good_time = secondary_dbm.lastGoodStamp()
+        primary_dbm = weewx.manager.open_manager_with_config(self.config_dict, primarydb_binding)
+        records = primary_dbm.genBatchRecords(last_good_time)
+        secondary_dbm.addRecord(records)
+        primary_dbm.close()
+        print("done")
