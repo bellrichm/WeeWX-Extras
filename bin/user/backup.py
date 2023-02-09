e271be094078c7e9633a6c576c4964354f15d73a
Rich Bell <bellrichm@gmail.com>
2023-02-09 09:38:27 -0500

    when running 'stand alone', allow the enable flag to be overridden

--- bin/user/backup.py
+++ bin/user/backup.py
@@ -298,6 +298,8 @@ if __name__ == "__main__":
         parser = argparse.ArgumentParser(usage=usage)
         parser.add_argument("--force-backup", action="store_true", dest="force_backup",
                             help="Force the backup to run.")
+        parser.add_argument("--enable", action="store_true", dest="enable",
+                            help="Override the configuration'enable' flag.")
         parser.add_argument("config_file")
 
         options = parser.parse_args()
@@ -307,6 +309,8 @@ if __name__ == "__main__":
 
         if options.force_backup:
             config_dict.merge({'Backup': {'force_backup': True}})
+        if options.enable:
+            config_dict.merge({'Backup': {'enable': True}})
 
         weeutil.logger.setup('wee_backup', config_dict)
 
