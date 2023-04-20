521c88034a47ee71a424ab71a98720292bf80f74
Rich Bell <bellrichm@gmail.com>
2023-04-20 08:11:13 -0400

    ability to have additional customizations

--- utils/buildConfig.py
+++ utils/buildConfig.py
@@ -17,21 +17,26 @@ if __name__ == '__main__': # pragma: no cover
                             help="The secrets file (password, API keys, etc).")
         parser.add_argument("--template", required=True, dest="template_config_file",
                             help="The template file.")
-        #parser.add_argument("--config", required=True, dest="config_file",
-        #                    help="The WeeWX config file.")
+        parser.add_argument("--add", dest="customization_file",
+                            help="Additional customizations.")
         parser.add_argument("config_file")
 
         options = parser.parse_args()
+        
+        template_config = configobj.ConfigObj(options.template_config_file, encoding='utf-8', interpolation=False, file_error=True)
+        
+        if options.customization_file:
+            customization_config = configobj.ConfigObj(options.customization_file, encoding='utf-8', interpolation=False, file_error=True)
+            template_config.merge(customization_config)
+                
+        secrets_config = configobj.ConfigObj(options.secrets_config_file, encoding='utf-8', interpolation=False, file_error=True)                
+        template_config.merge(secrets_config)
 
         if os.path.exists(options.config_file + ".bkup"):
             shutil.move(options.config_file + ".bkup", options.config_file + time.strftime(".bkup%Y%m%d%H%M%S"))
 
         if os.path.exists(options.config_file):
             shutil.move(options.config_file, options.config_file + ".bkup")
-
-        secrets_config = configobj.ConfigObj(options.secrets_config_file, encoding='utf-8', interpolation=False, file_error=True)
-        template_config = configobj.ConfigObj(options.template_config_file, encoding='utf-8', interpolation=False, file_error=True)
-        template_config.merge(secrets_config)
 
         template_config.filename = options.config_file
         template_config.write()
