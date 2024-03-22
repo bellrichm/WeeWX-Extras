f2e0ec764372e0b08b0df116499dd8b321afbf3e
Rich Bell <bellrichm@gmail.com>
2024-03-22 11:28:02 -0400

    more repos

--- utils/listVersions.py
+++ utils/listVersions.py
@@ -1,37 +1,47 @@
 #
 import subprocess
 repos_dir = '/home/pi/weewx_code/'
-repos = ['vds-weewx-v3-mem-extension', 'weewx-cmon', 'WeeWX-MQTTSubscribe', 'weewx-mqttpublish', 'weewx-jas', 'weewx-aqi-xtype', 'weewx-pushover', 'configs', 'WeeWX-Extras', 'secrets', 'weewx-healthchecks']
+repos = ['vds-weewx-v3-mem-extension', 'weewx-cmon', 'WeeWX-MQTTSubscribe', 'weewx-mqttpublish', 'weewx-jas', 'weewx-aqi-xtype', 'weewx-pushover', 'configs', 'WeeWX-Extras', 'secrets', 'weewx-healthchecks', 'weewx-gw1000']
 
 command = ['git', 'fetch', '--quiet']
 process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd='/home/pi/weewx')
 output, unused_err = process.communicate()
-print(f'weewx: {output.decode("utf-8")}')
+print(f'\nweewx: {output.decode("utf-8")}')
 
 for repo in repos:
-    cwd = f'{repos_dir}{repo}'
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=cwd)
-    output, unused_err = process.communicate()
-    print(f'{repo}: {output.decode("utf-8")}')
+    try:
+        cwd = f'{repos_dir}{repo}'
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=cwd)
+        output, unused_err = process.communicate()
+        print(f'{repo}: {output.decode("utf-8")}')
+    except FileNotFoundError:
+        print(f'{repo} does not exist')
 
 command = ['git', 'status', '--short', '-uno']
 process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd='/home/pi/weewx')
 output, unused_err = process.communicate()
-print(f'weewx: {output.decode("utf-8")}')
+print(f'\nweewx: {output.decode("utf-8")}')
 
 for repo in repos:
-    cwd = f'{repos_dir}{repo}'
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=cwd)
-    output, unused_err = process.communicate()
-    print(f'{repo}: {output.decode("utf-8")}')
+    try:
+        cwd = f'{repos_dir}{repo}'
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=cwd)
+        output, unused_err = process.communicate()
+        print(f'{repo}: {output.decode("utf-8")}')
+    except FileNotFoundError:
+        print(f'{repo} does not exist')
 
 command = ['git', 'log', '--format=format:"%ci %h %d %s"', '-n 1']
 process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd='/home/pi/weewx')
 output, unused_err = process.communicate()
-print(f'weewx: {output.decode("utf-8")}')
+print(f'\nweewx: {output.decode("utf-8")}')
 
 for repo in repos:
-    cwd = f'{repos_dir}{repo}'
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=cwd)
-    output, unused_err = process.communicate()
-    print(f'{repo}: {output.decode("utf-8")}')
+    try:
+        cwd = f'{repos_dir}{repo}'
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=cwd)
+        output, unused_err = process.communicate()
+        print(f'{repo}: {output.decode("utf-8")}')
+    except FileNotFoundError:
+        print(f'{repo} does not exist')
+
