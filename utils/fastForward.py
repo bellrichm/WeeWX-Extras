803fc387c27841d11b0a63d21e96a8683d57dd14
Rich Bell <bellrichm@gmail.com>
2024-05-21 09:35:14 -0400

    utility to fast forward all git repositories that are used

---
+++ utils/fastForward.py
@@ -0,0 +1,23 @@
+#
+import subprocess
+repos_dir = '/home/richbell/weewx_code/'
+weewx_dir = '/home/richbell/weewx'
+repos = ['weewx-gw1000',  'weewx-aqi-xtype', 'WeeWX-Extras', 'weewx-healthchecks', 'weewx-jas', 'weewx-mqttpublish', 'WeeWX-MQTTSubscribe', 'weewx-pushover', 'vds-weewx-v3-mem-extension', 'weewx-cmon', 'configs', 'secrets']
+
+command = ['git', 'pull', '--ff-only']
+print(f'\nRunning command: {" ".join(command)}')
+try:
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=weewx_dir)
+    output, unused_err = process.communicate()
+    print(f'weewx: {output.decode("utf-8")}')
+except FileNotFoundError:
+    print(f'{weewx_dir} does not exist')
+
+for repo in repos:
+    try:
+        cwd = f'{repos_dir}{repo}'
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=cwd)
+        output, unused_err = process.communicate()
+        print(f'{repo}: {output.decode("utf-8")}')
+    except FileNotFoundError:
+        print(f'{repo} does not exist')
