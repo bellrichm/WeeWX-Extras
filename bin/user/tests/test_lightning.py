df1d0f28cd401d76fb29195b455fad294e0fcc65
Rich Bell <bellrichm@gmail.com>
2024-08-26 16:29:47 -0400

    prep for unit tests

---
+++ bin/user/tests/test_lightning.py
@@ -0,0 +1,13 @@
+#
+#    Copyright (c) 2024 Rich Bell <bellrichm@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+import unittest
+
+class TestFirstLoopPacket(unittest.TestCase):
+    def test_lightning_count_is_total(self):
+        print("start")
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
