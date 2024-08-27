13e60c12c1efbcc660c4e06260ab8707769171f0
Rich Bell <bellrichm@gmail.com>
2024-08-27 17:13:22 -0400

    test new min

--- bin/user/tests/test_lightning.py
+++ bin/user/tests/test_lightning.py
@@ -17,8 +17,6 @@ import weewx
 
 class TestFirstLoopPacket(unittest.TestCase):
     def test_lightning_count_is_delta(self):
-        print("start")
-
         mock_engine = mock.Mock()
         config_dict = {
             'Lightning': {
@@ -49,8 +47,6 @@ class TestFirstLoopPacket(unittest.TestCase):
         self.assertEqual(event.packet[SUT.min_distance_field_name], strike_distance)
         self.assertEqual(event.packet[SUT.min_det_time_field_name], now)
 
-        print("done")
-
     def test_lightning_count_is_total(self):
         mock_engine = mock.Mock()
         now = int(time.time())
@@ -74,6 +70,47 @@ class TestFirstLoopPacket(unittest.TestCase):
         self.assertEqual(event.packet[SUT.last_det_time_field_name], now)
         self.assertEqual(event.packet[SUT.first_distance_field_name], strike_distance)
         self.assertEqual(event.packet[SUT.first_det_time_field_name], now)
+        self.assertEqual(event.packet[SUT.min_distance_field_name], strike_distance)
+        self.assertEqual(event.packet[SUT.min_det_time_field_name], now)
+
+    def test_new_min_value(self):
+        mock_engine = mock.Mock()
+        config_dict = {
+            'Lightning': {
+                'contains_total': False,
+            }
+        }
+        now = int(time.time())
+        strike_count = random.randint(100, 255)
+        strike_distance = random.randint(1, 50)
+
+        prior_strike_distance = strike_distance + random.randint(1, 50)
+        prior_strike_time = now - 60 * 60
+
+        SUT = user.lightning.Lightning(mock_engine, config_dict)
+
+        SUT.last_strike_distance = prior_strike_distance
+        SUT.last_strike_time = prior_strike_time
+        SUT.first_strike_distance = prior_strike_distance
+        SUT.first_strike_time = prior_strike_time
+        SUT.min_strike_distance = prior_strike_distance
+        SUT.min_strike_time = prior_strike_time
+
+        event = weewx.NEW_LOOP_PACKET()
+        event.packet = {
+            'dateTime': now,
+            SUT.strike_count_field_name: strike_count,
+            SUT.strike_distance_field_name: strike_distance,
+        }
+        SUT.new_loop_packet(event)
+
+        self.assertIsNone(SUT.strike_count_total)
+        self.assertEqual(event.packet[SUT.lightning_count_field_name], strike_count)
+        self.assertEqual(event.packet[SUT.lightning_distance_field_name], strike_distance)
+        self.assertEqual(event.packet[SUT.last_distance_field_name], strike_distance)
+        self.assertEqual(event.packet[SUT.last_det_time_field_name], now)
+        self.assertEqual(event.packet[SUT.first_distance_field_name], prior_strike_distance)
+        self.assertEqual(event.packet[SUT.first_det_time_field_name], prior_strike_time)
         self.assertEqual(event.packet[SUT.min_distance_field_name], strike_distance)
         self.assertEqual(event.packet[SUT.min_det_time_field_name], now)
 
