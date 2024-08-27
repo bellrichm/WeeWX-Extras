16020f5cb4e84e7e19b068b37cbb1242b5da15c1
Rich Bell <bellrichm@gmail.com>
2024-08-27 16:26:51 -0400

    test for delta strike count

--- bin/user/tests/test_lightning.py
+++ bin/user/tests/test_lightning.py
@@ -16,9 +16,42 @@ import user.lightning
 import weewx
 
 class TestFirstLoopPacket(unittest.TestCase):
-    def test_lightning_count_is_total(self):
+    def test_lightning_count_is_delta(self):
         print("start")
 
+        mock_engine = mock.Mock()
+        config_dict = {
+            'Lightning': {
+                'contains_total': False,
+            }
+        }
+        now = int(time.time())
+        strike_count = random.randint(1, 255)
+        strike_distance = random.randint(1, 50)
+
+        SUT = user.lightning.Lightning(mock_engine, config_dict)
+
+        event = weewx.NEW_LOOP_PACKET()
+        event.packet = {
+            'dateTime': now,
+            SUT.strike_count_field_name: strike_count,
+            SUT.strike_distance_field_name: strike_distance,
+        }
+        SUT.new_loop_packet(event)
+
+        self.assertIsNone(SUT.strike_count_total)
+        self.assertEqual(event.packet[SUT.lightning_count_field_name], strike_count)
+        self.assertEqual(event.packet[SUT.lightning_distance_field_name], strike_distance)
+        self.assertEqual(event.packet[SUT.last_distance_field_name], strike_distance)
+        self.assertEqual(event.packet[SUT.last_det_time_field_name], now)
+        self.assertEqual(event.packet[SUT.first_distance_field_name], strike_distance)
+        self.assertEqual(event.packet[SUT.first_det_time_field_name], now)
+        self.assertEqual(event.packet[SUT.min_distance_field_name], strike_distance)
+        self.assertEqual(event.packet[SUT.min_det_time_field_name], now)
+
+        print("done")
+
+    def test_lightning_count_is_total(self):
         mock_engine = mock.Mock()
         now = int(time.time())
         strike_count = random.randint(1, 255)
@@ -35,16 +68,14 @@ class TestFirstLoopPacket(unittest.TestCase):
         SUT.new_loop_packet(event)
 
         self.assertEqual(SUT.strike_count_total, strike_count)
-        self.assertIsNone(event.packet[SUT.strike_count_field_name])
-        self.assertIsNone(event.packet[SUT.strike_distance_field_name])
+        self.assertIsNone(event.packet[SUT.lightning_count_field_name])
+        self.assertIsNone(event.packet[SUT.lightning_distance_field_name])
         self.assertEqual(event.packet[SUT.last_distance_field_name], strike_distance)
         self.assertEqual(event.packet[SUT.last_det_time_field_name], now)
         self.assertEqual(event.packet[SUT.first_distance_field_name], strike_distance)
         self.assertEqual(event.packet[SUT.first_det_time_field_name], now)
         self.assertEqual(event.packet[SUT.min_distance_field_name], strike_distance)
         self.assertEqual(event.packet[SUT.min_det_time_field_name], now)
-
-        print("done")
 
 if __name__ == '__main__':
     unittest.main(exit=False)
