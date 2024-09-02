d2b773b68dc5586040687484b4f566ef34df0299
Rich Bell <bellrichm@gmail.com>
2024-09-02 10:42:44 -0400

    add some documentation

--- bin/user/observationtime.py
+++ bin/user/observationtime.py
@@ -5,7 +5,92 @@
 #
 
 '''
-WeeWX service to capture times and values for the first, last, min, and max of an observation.
+WeeWX service to capture times and values for the first, last, min, and max of an observation in an archive period.
+
+Installation:
+    1. Put this file in the bin/user directory.
+    2. Update weewx.conf [ObservationTime] as needed to configure the service.
+    3. Add the service, user.observationtime.ObservationTime to the appropriate 'service group'.
+       This would typically be the 'data_services' group.
+       But it might be the 'process_services' group if it needs to run after any of the WeeWX services in this group.
+
+Overview:
+For a configured WeeWX observation, capture any of the last, first, min, or max values in an archive period. 
+In addition to capturing the value, the time that the value was observed is also captured.
+These values and times are added to the loop packet.
+The WeeWX accumulator function is used to populate the archive record with these values.
+
+Configuration:
+[ObservationTime]
+    [[observations]]
+        # The first observation whose 'event' value and time will be captured.
+        # For example: lightning_distance, windGust, etc.
+        [[[REPLACE_ME]]]
+            [[[[last]]]]
+                # The name of the WeeWX to store the last observation's value.
+                # For example: lightning_last_distance, windGust_last, etc.
+                observation_name
+                # The name of the WeeWX to store the last observation's time.
+                # For example: lightning_last_det_time, windGust_last_time, etc.                
+                observation_time
+            [[[[first]]]]
+            # The name of the WeeWX to store the first observation's value.
+                # For example: lightning_first_distance, windGust_first, etc.            
+                observation_name
+                # The name of the WeeWX to store the first observation's time.
+                # For example: lightning_first_det_time, windGust_first_time, etc.                                
+                observation_time
+            [[[[min]]]]
+                # The name of the WeeWX to store the min observation's value.
+                # For example: lightning_min_distance, windGust_min, etc.            
+                observation_name
+                # The name of the WeeWX to store the min observation's time.
+                # For example: lightning_min_det_time, windGust_min_time, etc.                                
+                observation_time
+            [[[[max]]]]
+                # The name of the WeeWX to store the max observation's value.
+                # For example: lightning_max_distance, windGust_max, etc.            
+                observation_name
+                # The name of the WeeWX to store the max observation's time.
+                # For example: lightning_max_det_time, windGust_max_time, etc.                                
+                observation_time =
+        # The next observation whose 'event' value and time will be captured.
+        [[[REPLACE_ME_TOO]]]
+                
+Below is an example of a 'complete' configuration to capture the last, first, and closest lightning strike in an archive period.
+This assumes that in the WeeWX loop packet, the 'lightning_distance' field captures the distance to the strike.
+[Engine]
+    [[Services]]
+        data_services = user.observationtime.ObservationTime
+
+[Accumulator]
+    [[lightning_last_distance]]
+        extractor = last
+    [[lightning_last_det_time]]
+        extractor = last
+    # Note, extractor = last would work because the first detection is added to every loop by the ObservationTime service.
+    [[lightning_first_distance]]
+        extractor = first
+    [[lightning_first_det_time]]
+        extractor = first
+    [[lightning_min_distance]]
+        extractor = last
+    [[lightning_min_det_time]]
+        extractor = last
+
+[ObservationTime]
+    [[observations]]
+        [[[lightning_distance]]]
+            [[[[last]]]]
+                observation_name = lightning_last_distance
+                observation_time = lightning_last_det_time
+            [[[[first]]]]
+                observation_name = lightning_first_distance
+                observation_time = lightning_first_det_time
+            [[[[min]]]]
+                observation_name = lightning_min_distance
+                observation_time = lightning_min_det_time
+
 '''
 
 import logging
@@ -13,11 +98,14 @@ import logging
 import weewx
 import weewx.engine
 
+VERSION = '0.1.0'
+
 log = logging.getLogger(__name__)
 
 class ObservationTime(weewx.engine.StdService):
     ''' Save the times and values of the first, last, min, and max of an observation.'''
     def __init__(self, engine, config_dict):
+        log.info("Version is: %s", VERSION)
         super(ObservationTime, self).__init__(engine, config_dict)
 
         # ToDo: perform a deep copy
