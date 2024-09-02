354761be5beeb475d3527ccfe907bc588fb67e61
Rich Bell <bellrichm@gmail.com>
2024-09-02 11:15:12 -0400

    logging cleanup

--- bin/user/observationtime.py
+++ bin/user/observationtime.py
@@ -124,44 +124,41 @@ class ObservationTime(weewx.engine.StdService):
 
     def new_loop_packet(self, event):
         ''' Handle the WeeWX POST_LOOP event.'''
-        date_time = event.packet['dateTime']
-        log.info("checking: %s", event.packet)
+        log.debug("Incoming packet is: %s", event.packet)
         for observation, observation_data in self.observations.items():
             if observation not in event.packet:
                 continue
 
-            log.info("processing observation: %s %s", observation, observation_data)
-            log.info("processing: %s", event.packet)
+            log.debug("Processing observation: %s %s", observation, observation_data)
             observation_value = event.packet[observation]
+            date_time = event.packet['dateTime']
+
             for observation_type in observation_data:
-                log.info("processing type: %s", observation_type)
-                log.info("processing prev value: %s", observation_data[observation_type]['observation'])
-                log.info("processing prev time: %s", observation_data[observation_type]['observation_time'])
-                log.info("processing curr value: %s", observation_value)
-                log.info("processing curr time: %s", date_time)
+                previous_value = observation_data[observation_type]['observation']
+                previous_time = observation_data[observation_type]['observation_time']
+
+                log.debug("Processing type: %s", observation_type)
+                log.debug("Processing previous value: %s and previous time: %s", previous_value, previous_time)
+                log.debug("Processing current value: %s and current time: %s", observation_value, date_time)
 
                 if observation_type == 'last':
-                    self._set_values(observation_data, observation_type, observation_value, date_time)
+                    self._set_values(observation_data, observation, observation_type, observation_value, date_time)
 
-                if observation_type == 'first' and observation_data[observation_type]['observation'] is None:
-                    self._set_values(observation_data, observation_type, observation_value, date_time)
+                if observation_type == 'first' and previous_value is None:
+                    self._set_values(observation_data, observation, observation_type, observation_value, date_time)
 
-                if observation_type == 'min' and \
-                    (observation_data[observation_type]['observation'] is None or \
-                     observation_value <= observation_data[observation_type]['observation']):
-                    self._set_values(observation_data, observation_type, observation_value, date_time)
+                if observation_type == 'min' and (previous_value is None or observation_value <= previous_value):
+                    self._set_values(observation_data, observation, observation_type, observation_value, date_time)
 
-                if observation_type == 'max' and \
-                    (observation_data[observation_type]['observation'] is None or \
-                     observation_value >= observation_data[observation_type]['observation']):
-                    self._set_values(observation_data, observation_type, observation_value, date_time)
+                if observation_type == 'max' and (previous_value is None or observation_value >= previous_value):
+                    self._set_values(observation_data, observation, observation_type, observation_value, date_time)
 
                 event.packet[observation_data[observation_type]['observation_name']] = observation_data[observation_type]['observation']
                 event.packet[observation_data[observation_type]['observation_time_name']] =\
                       observation_data[observation_type]['observation_time']
 
-    def _set_values(self, observation_data, observation_type, observation_value, date_time):
-        log.info("processing Setting %s observation %s and time %s", observation_type, observation_value, date_time)
+    def _set_values(self, observation_data, observation, observation_type, observation_value, date_time):
+        log.debug("Setting %s of %s value %s and time %s", observation_type, observation, observation_value, date_time)
         observation_data[observation_type]['observation'] = observation_value
         observation_data[observation_type]['observation_time'] = date_time
     
