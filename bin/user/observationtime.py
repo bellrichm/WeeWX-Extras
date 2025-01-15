86ba452c03b8c92517e98da1be2afdf0c31e2cf6
Rich Bell <bellrichm@gmail.com>
2025-01-15 10:20:42 -0500

    linting

--- bin/user/observationtime.py
+++ bin/user/observationtime.py
@@ -194,8 +194,8 @@ class ObservationTime(weewx.engine.StdService):
             for key, value in observation_data['data'].items():
                 observation_time =  value['time']
                 observation_value =  value['value']
-                
-                # Observation value belongs to previous archive record, 
+
+                # Observation value belongs to previous archive record,
                 if observation_time <= start_timestamp:
                     del observation_data['data'][key]
                     continue
